74b92d0c9e19af06ff36bb7e55eadab242853b77
Jordan Davis <jordan.m.davis92@gmail.com>
2019-04-25 19:27:46 -0400

    outlets
    created subclasses for the UITableViewController and connected outlets to the stack views in the Questions Table View Cell

---
+++ Q&A/Q&A/QuestionsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionsTableViewCell.swift
+//  Q&A
+//
+//  Created by Jordan Davis on 4/25/19.
+//  Copyright © 2019 Jordan Davis. All rights reserved.
+//
+
+import UIKit
+
+class QuestionsTableViewCell: UITableViewCell {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var askerLabel: UILabel!
+    @IBOutlet var answerableLabel: UILabel!
+    
+}
